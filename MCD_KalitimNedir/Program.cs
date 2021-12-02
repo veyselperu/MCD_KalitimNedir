35b372213a93a16360b7e64a506e73d14479a8b2
Faik Turan <turanfaik@hotmail.com>
2021-12-02 17:43:02 +0300

    Add project files.

---
+++ MCD_KalitimNedir/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KalitimNedir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Kalıtım Miras Alma Nedir
+             * Kalıtım yolu ile oluşturmuş olduğumuz nesnelerimiz birbilerinden türetilebilir ve türeyen nesne türetilen nesnenin tüm özelliklerini kullanabilir.
+             * C# programlama diline başlarken tip dönüşümlerinde tüm nesneleri tanıyan her türden nesneyi kabul eden object tipinden bahsetmiştik.
+             * Object tipi framework içerisindeki tüm nesnelerin atası olan bir tip olduğu için (tüm nesneler object nesnesinden kalıtıldığı için) türetilen tip kendisinden türeyen tüm tipleri tanır ve tam bir uyumluluk sağlar.
+             * Ogrenci O1 = new Ogrenci();
+             * Nesne örneği alınan öğrenci nesnesi aslında object tipinden türetilmiş bir nesnedir.
+             * 
+             * 
+             * 
+             * 
+             * 
+             */ 
+        }
+    }
+}
