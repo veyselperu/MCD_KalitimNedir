c874eec0ec1063f72795bcb9b395261a26942c73
Faik Turan <turanfaik@hotmail.com>
2021-12-02 18:04:31 +0300

    code changed

---
+++ MCD_KalitimNedir/Egitmen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KalitimNedir
+{
+    public class Egitmen:temelTip
+    {
+        public string Brans { get; set; }
+        public int BransSeviye { get; set; }
+
+        public Egitmen()
+        {
+            Console.WriteLine("Egitmen nesnesinin yapıcı metotu çalıştı");
+            //IdAtamaIslemi
+        }
+    }
+}
---
+++ MCD_KalitimNedir/Ogrenci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KalitimNedir
+{
+    public class Ogrenci:temelTip
+    {
+        public string bolum { get; set; }
+        public int seviye { get; set; }
+
+        public Ogrenci()
+        {
+            Console.WriteLine("Ogrenci nesnesi çalıştı");
+            //TemelTipMetot2(); protected olarak işaretlendiği için bu nesne kendisini temeltip nesnesinden kalıttığı için protected seviyesinde olan field veya metotlara erişebilir.
+        }
+    }
+}
