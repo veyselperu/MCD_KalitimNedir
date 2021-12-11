7d0377af361f0e1e6c2c5426b403afda6070a6fe
Waysel Peru <veyselperu@gmail.com>
2021-12-11 09:15:49 +0300

    Update Program.cs

--- MCD_KalitimNedir/Program.cs
+++ MCD_KalitimNedir/Program.cs
@@ -10,7 +10,7 @@ namespace MCD_KalitimNedir
     {
         static void Main(string[] args)
         {
-            /* Kalıtım Miras Alma Nedir
+            /* Kalıtım Miras Alma Nedir?
              * Kalıtım yolu ile oluşturmuş olduğumuz nesnelerimiz birbilerinden türetilebilir ve türeyen nesne türetilen nesnenin tüm özelliklerini kullanabilir.
              * C# programlama diline başlarken tip dönüşümlerinde tüm nesneleri tanıyan her türden nesneyi kabul eden object tipinden bahsetmiştik.
              * Object tipi framework içerisindeki tüm nesnelerin atası olan bir tip olduğu için (tüm nesneler object nesnesinden kalıtıldığı için) türetilen tip kendisinden türeyen tüm tipleri tanır ve tam bir uyumluluk sağlar.
